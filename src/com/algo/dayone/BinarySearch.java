a171c0135dde4dcd68db9a2d9117ecfec2cdb810
MohammedHarfan <64004968+MohammedHarfan@users.noreply.github.com>
2022-05-20 18:10:46 +0530

    Update BinarySearch.java

--- src/com/algo/dayone/BinarySearch.java
+++ src/com/algo/dayone/BinarySearch.java
@@ -26,7 +26,7 @@ public class BinarySearch {
 }
 
 /*
-Question:
+Question:(Binary Search)
 Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
 You must write an algorithm with O(log n) runtime complexity.
 
